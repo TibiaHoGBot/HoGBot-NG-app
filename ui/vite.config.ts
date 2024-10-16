fd9c6622d420c29cf5537b894e6222deb4cc3ec0
szulak <4438359+aszulak@users.noreply.github.com>
2024-10-16 14:43:07 +0200

    Update vite.config.ts

--- ui/vite.config.ts
+++ ui/vite.config.ts
@@ -1,9 +1,10 @@
 import { defineConfig } from 'vite'
 import { svelte } from '@sveltejs/vite-plugin-svelte'
+import { viteSingleFile } from "vite-plugin-singlefile"
 import path from "path"
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [svelte()],
+  plugins: [svelte(), viteSingleFile()],
   resolve: {
     alias: {
       $lib: path.resolve('./src/lib')
